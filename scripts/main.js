5710eab61c55b41f6e68eea84731a781f59daec8
Mars L <marialeyva782@gmail.com>
2023-07-26 09:53:49 -0400

    generates Random Word for game

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,26 @@
 const playButton = document.getElementById(`playButton`);
-const fullGame = document.getElementById(`theGame`);
-const gameHeading = document.getElementById(`gameHeader`);
+
 
 playButton.addEventListener(`click`, function () {
+     
+    const fullGame = document.getElementById(`theGame`);
+    const gameHeading = document.getElementById(`gameHeader`);
+   
     fullGame.style.display = `block`;
     playButton.style.display = `none`;
     gameHeading.innerHTML = `Guess A Letter!`;
+    // alert(`Let the games begin`);
 });
+
+
+// This section of code is just testing Keyboard
+let a = alphabet.A;
+function pickWord() {
+    let wordChoice = [`lobster`, `lover`, `girlie`,`lodgreen`];
+    let randomChoice = Math.floor( Math.random() * wordChoice.length);
+    // console.log(wordChoice[randomChoice]);
+    return wordChoice[randomChoice];
+}
+
+let word = pickWord();
+console.log(word);
