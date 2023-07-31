4ef98ec1c749e2beb9eef000fd3ccdf5121f64a1
Mars L <marialeyva782@gmail.com>
2023-07-31 08:40:49 -0400

    display of Correct vs Incorrect letter choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-const playButton = document.getElementById(`playButton`);
-let pickedLetter;
 let alphabet = {
     A : document.getElementById(`a`),
     B : document.getElementById(`b`),
@@ -28,23 +26,26 @@ let alphabet = {
     Y : document.getElementById(`y`),
     Z : document.getElementById(`z`)
 };
-
+let incorrectGuesses = [];
+let correctGuesses = [];
 
 function pickWord() {
     let wordChoice = [
-        `html`, `april`, `responsive`, `spanish`, `peru`,
-        `cuba`, `pastries`, `starbucks`, `paint`
+        `html`, `april`,  `peru`,
+        `cuba`, `pastry`, `sbux`, `paint`
     ];
     let randomChoice = Math.floor( Math.random() * wordChoice.length);
     return wordChoice[randomChoice];
 }
 
-const selectedWord = pickWord();
+let selectedWord = pickWord();
+const playButton = document.getElementById(`playButton`);
 
 function displayUnderlines() {
     for(let i = 0; i < selectedWord.length; i++) {
         let node = document.createElement(`p`);
         let textnode = document.createTextNode(`${selectedWord[i]}`);
+        node.setAttribute(`id`, `${selectedWord[i]}`);
         node.appendChild(textnode);    
         document.getElementById(`underlines`).appendChild(node);
     }        
@@ -53,22 +54,46 @@ function displayUnderlines() {
 function clickedLetter() {
     for (let letter in alphabet) {
         alphabet[letter].addEventListener(`click`, function () {
-            pickedLetter = alphabet[letter].innerHTML;
-            console.log(pickedLetter);
-            return pickedLetter;
-        });
+            let pickedLetter = alphabet[letter].innerHTML;
+            let editLetter = document.getElementById(`${pickedLetter}`);
+
+            for(let i = 0; i < selectedWord.length; i++) {    
+                if(pickedLetter === selectedWord[i] 
+                        && !correctGuesses.includes(pickedLetter)) {
+                    editLetter.style.color = `black`; 
+                    alphabet[letter].style.color = `white`; 
+                    alphabet[letter].style.backgroundColor = `lightgreen`;
+                    alphabet[letter].style.borderColor = `green`;
+                    alphabet[letter].style.boxShadow = `none`;
+                    correctGuesses.push(pickedLetter);
+                    if(correctGuesses.length == selectedWord.length) {
+                        alert(`You won!`);
+                    }
+                }
+                
+                if(!selectedWord.includes(pickedLetter) 
+                && !incorrectGuesses.includes(pickedLetter)){
+                    incorrectGuesses.push(pickedLetter);
+                    alphabet[letter].style.color = `white`; 
+                    alphabet[letter].style.backgroundColor = `grey`; 
+                    alphabet[letter].style.boxShadow = `none`;
+                    document.getElementById(`wrongLetters`)
+                        .innerHTML= incorrectGuesses.toString();
+                }
+            } 
+        }); 
     }
 }
 
 playButton.addEventListener(`click`, function () {
     const fullGame = document.getElementById(`theGame`);
     const gameHeading = document.getElementById(`gameHeader`);
-   
+
     fullGame.style.display = `block`;
     playButton.style.display = `none`;
     gameHeading.innerHTML = `Guess A Letter!`;
     displayUnderlines();
     clickedLetter();
+   
 });
-
 
